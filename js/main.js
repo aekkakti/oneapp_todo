76c419ada3ad61cf749db2a6558adb9af2a57e0b
aekkakti <music.tomsk1@gmail.com>
2024-02-07 12:55:45 +0700

    Не кликается задача

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 new Vue({
     el: '#todo',
     data: {
+        cards: [],
         column1Cards: [],
         column2Cards: [],
-        column3Cards: []
+        column3Cards: [],
+        name: '',
+        countCards: 0,
+        completeTaskPercent: 0
+    },
+    methods: {
+        addCard() {
+            if (this.countCards < 3 && this.name !== '') {
+                let newCard = {name: this.name, tasks: []}
+                this.column1Cards.push(newCard)
+                this.countCards += 1
+            }
+            else if (this.name === '') {
+                alert('Нельзя создавать пустую карточку!')
+            }
+            else {
+                alert('Максимум можно добавить только 3 карточки!')
+            }
+        },
+        addTask(cardIndex, newTask) {
+            this.cards[cardIndex].tasks.push(newTask);
+            console.log(this.cards)
+        },
+        completeTask(cardIndex, taskIndex) {
+            this.cards[cardIndex].tasks[taskIndex].completeStyle = !this.cards[cardIndex].tasks[taskIndex].completeStyle
+            const completedTasks = this.cards[cardIndex].tasks.filter(task => task.completeStyle)
+            this.completeTaskPercent = 100 / this.cards[cardIndex].tasks.length * completedTasks.length
+        },
     }
 })
