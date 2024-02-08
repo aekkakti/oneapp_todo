2e130b2e4fb3a6c406016f04c5a06039b632f9e5
aekkakti <music.tomsk1@gmail.com>
2024-02-08 10:33:25 +0700

    Осталось перенести из второй карточки в третью

--- js/main.js
+++ js/main.js
@@ -6,16 +6,17 @@ new Vue({
         column3Cards: [],
         cardName: '',
         taskName: '',
-        countCards: 0,
+        countColumnCard1: 0,
+        countColumnCard2: 0,
         countTasks: 0,
         completeTaskPercent: 0
     },
     methods: {
         addCard() {
-            if (this.countCards < 3 && this.cardName !== '') {
+            if (this.countColumnCard1 < 3 && this.cardName !== '') {
                 let newCard = {cardName: this.cardName, tasks: [], taskName: ''}
                 this.column1Cards.push(newCard)
-                this.countCards += 1
+                this.countColumnCard1 += 1
             }
             else if (this.cardName === '') {
                 alert('Нельзя создавать пустую карточку!')
@@ -29,7 +30,7 @@ new Vue({
                 let newTask = {taskName: this.column1Cards[cardIndex].taskName, completeStyle: false}
                 this.column1Cards[cardIndex].tasks.push(newTask)
                 this.column1Cards[cardIndex].taskName = ''
-                this.countTasks += 1
+                this.countColumnCard1 += 1
             }
             else if (this.column1Cards[cardIndex].taskName === '') {
                 alert('Нельзя создавать пустую задачу!')
@@ -38,11 +39,21 @@ new Vue({
                 alert('Максимум можно добавить только 5 задач!')
             }
         },
-        completeTask(newCard, cardIndex, taskIndex) {
+        completeTask(newCard, taskIndex, cardIndex) {
             newCard.tasks[taskIndex].completeStyle =  !newCard.tasks[taskIndex].completeStyle
             const completedTasks = newCard.tasks.filter(task => task.completeStyle)
             this.completeTaskPercent = 100 / newCard.tasks.length * completedTasks.length
-            console.log(this.completeTaskPercent)
-        },
+            if (this.completeTaskPercent >= 50 && this.countColumnCard2 < 5) {
+                this.column2Cards.push(this.column1Cards[cardIndex])
+                this.column1Cards.splice(cardIndex, 1)
+                this.countColumnCard1 -= 1
+                this.countColumnCard2 += 1
+            }
+            if (this.completeTaskPercent === 100) {
+                this.column3Cards.push(this.column2Cards[cardIndex])
+                this.column2Cards.splice(cardIndex, 1)
+                this.countColumnCard2 -= 1
+            }
+        }
     }
 })
