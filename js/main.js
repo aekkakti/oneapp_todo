8d0785706f5be38ff469ab650f9588f67b935953
aekkakti <music.tomsk1@gmail.com>
2024-02-09 09:33:38 +0700

    Счётчик теперь работает корректно, осталось реализовать: 1. Нормальную блокировку первого столбца 2. При создании карточки по умолчанию создавать 3 поля ввода для задач

--- js/main.js
+++ js/main.js
@@ -10,14 +10,15 @@ new Vue({
         countColumnCard2: 0,
         countTasks: 0,
         completeTaskPercent: 0,
+        isFirstColumnBlocked: false
     },
     methods: {
         addCard() {
             if (this.countColumnCard1 < 3 && this.cardName !== '') {
                 let newCard = {cardName: this.cardName, tasks: [], taskName: ''}
+                this.cardName = ''
                 this.column1Cards.push(newCard)
                 this.countColumnCard1 += 1
-                this.countTasks = 0
                 this.saveDataToLocalStorage()
             }
             else if (this.cardName === '') {
@@ -28,7 +29,7 @@ new Vue({
             }
         },
         addTask(cardIndex) {
-            if (this.column1Cards[cardIndex].taskName !== '' && (this.countTasks < 3 || this.countTasks < 5)) {
+            if (this.column1Cards[cardIndex].taskName !== '' && (this.countTasks < 3 || this.countTasks < 5) && !this.isFirstColumnBlocked) {
                 let newTask = {taskName: this.column1Cards[cardIndex].taskName, completeStyle: false}
                 this.column1Cards[cardIndex].tasks.push(newTask)
                 this.column1Cards[cardIndex].taskName = ''
@@ -47,19 +48,26 @@ new Vue({
             const completedTasks = newCard.tasks.filter(task => task.completeStyle)
             this.completeTaskPercent = 100 / newCard.tasks.length * completedTasks.length
             if (this.completeTaskPercent === 100) {
+                this.countColumnCard2 -= 1
                 this.column3Cards.push(this.column2Cards[cardIndex])
                 this.column2Cards.splice(cardIndex, 1)
-                this.countColumnCard2 -= 1
                 newCard.timeEnd = new Date().toLocaleString()
             }
-            if (this.completeTaskPercent >= 50 && this.countColumnCard2 < 5) {
-                this.column2Cards.push(this.column1Cards[cardIndex])
-                this.column1Cards.splice(cardIndex, 1)
+            else if (this.completeTaskPercent >= 50 && this.countColumnCard2 < 5 && !this.isFirstColumnBlocked) {
                 this.countColumnCard1 -= 1
                 this.countColumnCard2 += 1
+                this.column2Cards.push(this.column1Cards[cardIndex])
+                this.column1Cards.splice(cardIndex, 1)
+            }
+            if (this.countColumnCard2 > 0) {
+                this.isFirstColumnBlocked = true
+            }
+            else if (this.countColumnCard2 === 5 && this.completeTaskPercent >= 50) {
+                this.isFirstColumnBlocked = true
             }
             this.saveDataToLocalStorage()
         },
+
         saveDataToLocalStorage() {
             const dataToStore = {
                 column1Cards: this.column1Cards,
@@ -67,6 +75,7 @@ new Vue({
                 column3Cards: this.column3Cards,
                 countColumnCard1: this.countColumnCard1,
                 countColumnCard2: this.countColumnCard2,
+                isFirstColumnBlocked: this.isFirstColumnBlocked
             }
             localStorage.setItem('appData', JSON.stringify(dataToStore))
         },
@@ -78,5 +87,6 @@ new Vue({
         this.column3Cards = storedData.column3Cards
         this.countColumnCard1 = storedData.countColumnCard1
         this.countColumnCard2 = storedData.countColumnCard2
+        this.isFirstColumnBlocked = storedData.isFirstColumnBlocked
     }
 })
